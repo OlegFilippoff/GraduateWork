f3062eb23be0d3c230ba1b1e7c232348c6a6dec7
Filippoff Oleg <5157175@list.ru>
2022-12-03 11:17:56 +0300

    added dataBase interaction methods

---
+++ src/test/java/data/dataBase.java
@@ -0,0 +1,61 @@
+package data;
+
+import lombok.SneakyThrows;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class dataBase {
+
+    private static String url = System.getProperty("db.url");
+    private static String user = System.getProperty("db.user");
+    private static String password = System.getProperty("db.password");
+
+    @SneakyThrows
+    public static void clearTables() {
+        QueryRunner runner = new QueryRunner();
+        String deleteCredit = "DELETE FROM credit_request_entity";
+        String deleteOrder = "DELETE FROM order_entity";
+        String deletePayment = "DELETE FROM payment_entity";
+
+        try (
+                Connection connection = DriverManager.getConnection(
+                        url, user, password);
+
+        ) {
+            runner.update(connection, deleteCredit);
+            runner.update(connection, deleteOrder);
+            runner.update(connection, deletePayment);
+        }
+
+    }
+
+    @SneakyThrows
+    public static String getStatus(String status) {
+        QueryRunner runner = new QueryRunner();
+
+        try (
+                Connection connection = DriverManager.getConnection(
+                        url, user, password);
+
+        ) {
+            String result = runner.query(connection, status, new ScalarHandler<>());
+            System.out.println(result);
+            return result;
+        }
+    }
+
+    @SneakyThrows
+    public static String getStatusPayment() {
+        String statusSQL = "SELECT status FROM payment_entity ORDER BY created ASC LIMIT 1";
+        return getStatus(statusSQL);
+    }
+
+    @SneakyThrows
+    public static String getStatusCredit() {
+        String statusSQL = "SELECT status FROM credit_request_entity ORDER BY created ASC LIMIT 1";
+        return getStatus(statusSQL);
+    }
+}
